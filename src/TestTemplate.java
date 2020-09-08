03096d3aba5160373379e083721e3f475939c70e
sumitdeo <sumit.deo@worldpay.com>
2020-09-08 13:13:22 -0400

    If applied, this commit will add code for chapter 2 - item 3

--- src/TestTemplate.java
+++ src/TestTemplate.java
@@ -3,11 +3,11 @@
  * @projectName JoshuaBlochEffectiveJava
  * @package PACKAGE_NAME
  * @date 9/8/20
- * @comment:
+ * @comment: just a test template
  */
 public class TestTemplate {
 
     public static void main(String[] args) {
-        
+
     }
 }
