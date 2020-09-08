5ee7961126d08cfd873e0bca838df3b2b7833b5a
sumitdeo <sumit.deo@worldpay.com>
2020-09-08 14:34:09 -0400

    If applied, this commit will add code for chapter 2 - item 6

---
+++ src/com/sdeo/chapter2/item6/RomanNumerals.java
@@ -0,0 +1,46 @@
+package com.sdeo.chapter2.item6;
+
+import java.util.regex.Pattern;
+
+/**
+ * @author sumitdeo
+ * @projectName JoshuaBlochEffectiveJava
+ * @package com.sdeo.chapter2.item6
+ * @date 9/8/20
+ * @comment:
+ */
+public class RomanNumerals {
+
+    // Reusing expensive object for improved performance (Page 23)
+    private static final Pattern ROMAN = Pattern.compile(
+            "^(?=.)M*(C[MD]|D?C{0,3})"
+                    + "(X[CL]|L?X{0,3})(I[XV]|V?I{0,3})$");
+
+    static boolean isRomanNumeralSlow(String s) {
+        return s.matches("^(?=.)M*(C[MD]|D?C{0,3})"
+                + "(X[CL]|L?X{0,3})(I[XV]|V?I{0,3})$");
+    }
+
+    static boolean isRomanNumeralFast(String s) {
+        return ROMAN.matcher(s).matches();
+    }
+
+    public static void main(String[] args) {
+        int numSets = Integer.parseInt(args[0]);
+        int numReps = Integer.parseInt(args[1]);
+        boolean b = false;
+
+        for (int i = 0; i < numSets; i++) {
+            long start = System.nanoTime();
+            for (int j = 0; j < numReps; j++) {
+                b ^= isRomanNumeralFast("MCMLXXVI");  // Change Slow to Fast to see performance difference
+            }
+            long end = System.nanoTime();
+            System.out.println(((end - start) / (1_000. * numReps)) + " μs.");
+        }
+
+        // Prevents VM from optimizing away everything.
+        if (!b)
+            System.out.println();
+    }
+}
---
+++ src/com/sdeo/chapter2/item6/Sum.java
@@ -0,0 +1,33 @@
+package com.sdeo.chapter2.item6;
+
+/**
+ * @author sumitdeo
+ * @projectName JoshuaBlochEffectiveJava
+ * @package com.sdeo.chapter2.item6
+ * @date 9/8/20
+ * @comment:
+ */
+public class Sum {
+    private static long sum() {
+        Long sum = 0L;
+        for (long i = 0; i <= Integer.MAX_VALUE; i++)
+            sum += i;
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int numSets = Integer.parseInt(args[0]);
+        long x = 0;
+
+        for (int i = 0; i < numSets; i++) {
+            long start = System.nanoTime();
+            x += sum();
+            long end = System.nanoTime();
+            System.out.println((end - start) / 1_000_000. + " ms.");
+        }
+
+        // Prevents VM from optimizing away everything.
+        if (x == 42)
+            System.out.println();
+    }
+}
