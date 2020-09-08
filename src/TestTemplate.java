6d0f81f825ab858286e744240252ce2558957385
sumitdeo <sumit.deo@worldpay.com>
2020-09-08 13:12:06 -0400

    If applied, this commit will add code for chapter 2 - item 3

--- src/TestTemplate.java
+++ src/TestTemplate.java
@@ -1,5 +1,3 @@
-import com.sdeo.chapter2.item3.staticFactory.Elvis;
-
 /**
  * @author sumitdeo
  * @projectName JoshuaBlochEffectiveJava
@@ -10,7 +8,6 @@ import com.sdeo.chapter2.item3.staticFactory.Elvis;
 public class TestTemplate {
 
     public static void main(String[] args) {
-        Elvis elvis = Elvis.getInstance();
-        elvis.leaveTheBuilding();
+        
     }
 }
